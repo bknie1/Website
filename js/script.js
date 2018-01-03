e18996a893f3f6c010a74b632820eb170ca26dcc
bknie1 <brandonknieriem26@gmail.com>
2018-01-03 00:48:35 +0000

    Basic setup.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Hambuger Logic */
+
+(function () {
+  var burger = document.querySelector('.navbar-burger');
+  var menu = document.querySelector('.navbar-menu');
+  burger.addEventListener('click', function () {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+    });
+})();
